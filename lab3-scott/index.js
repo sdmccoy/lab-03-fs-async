6121a8bfc8721f5a5be1f6d8e8b939d4121dd21c
Scott <sdmccoy@gmail.com>
2017-06-14 21:49:31 -0700

    progress is now working. When calling node index.js and ./data/set1.txt', './data/set2.txt', './data/set3.txt directly after, it will print the files contents into an array.

--- lab3-scott/index.js
+++ lab3-scott/index.js
@@ -2,7 +2,10 @@
 
 const printFiles = require('./lib/print-files.js');
 
-let filePaths = ['./data/set1.txt', './data/set2.txt', './data/set3.txt'];
+let filePaths = process.argv.splice(2);
+
+// paths inputted in CLI that process.argv puts in to the printFileContent()
+// ['./data/set1.txt', './data/set2.txt', './data/set3.txt'];
 
 let printFileContent = () => {
   printFiles(filePaths, (err, data) => {
